979a7b51a4d51f7b7fdc1809c698a278fd6a391d
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-02-17 10:13:23 -0600

    added player name variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName + " is ready for battle!");
+function fight(){
+    window.alert("the fight has begun!");
+}
+
+//called functions
+//fight();
